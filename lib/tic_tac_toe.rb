a4f54507449f133ba12a47c043778b026ec13a83
Eddie Melo <heriberto@melo.nyc>
2017-11-21 16:05:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,10 @@
-
+WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [0,4,8], #Left diag
+  [2,4,6], #right diag
+  [0,3,6], #left column
+  [1,4,7], #middle column
+  [2,5,8] #right column
+]
